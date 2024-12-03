8a4617d5ccc4007039bf836bfa1e65d081d6b38c
KwesiLarbi <KwesiiiLarb@gmail.com>
2024-12-03 17:41:02 -0500

    created text to inform users of game rules / taking users name as input / prompting them if they are ready to start game, exits otherwise

--- rps.c
+++ rps.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /* 
     * #TODO: Inform Users that they have started up rock! paper! scissors!
     *        - Also inform the users of the rules of the game. 
@@ -11,7 +9,43 @@
     * #TODO: Keep track of User and CPU score while program is running
     *        - When program is terminated compare total scores and decide overall winner
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE_U1 30
+
 int main()
 {
-    printf("Hello Kwesi! This is the Rock! Paper! Scissors! (RPS) Game!\n");
+    /* User enters name to be greeted */
+    char u1[BUFFER_SIZE_U1];
+    char ready;
+    
+    printf("Please enter your name: \n");
+    if (fgets(u1, sizeof(u1), stdin) == NULL) {
+        printf("Failed to read the input stream!");
+    }
+    else {
+        u1[strcspn(u1, "\n")] = '\0';
+    }
+
+    printf("\n\nHello %s, and welcome to Rock! Paper! Scissors!\n\n", u1);
+    printf("Rules of RPS:\n");
+    printf("\t* Only options are rock(r), paper(p), and scissors(s)\n");
+    printf("\t* CPU will randomly generate r, p, or s");
+    printf("\t* Winning conditions are rock > scissors, scissors > paper, paper > rock\n");
+    printf("\t* After each round a score will be kept and displayed\n");
+    printf("\t* When program is exited an overall winner be declared based on point totals\n\n");
+
+    printf("Are you ready? [y | n]: \n");
+    if ((ready = getchar()) != 'y') {
+        printf("Okay bye bye!\n");
+        exit(1);
+    }
+
+    // TODO: Create while loop with switch statement
+    printf("\nRock? Paper? Scissors? [r | p | s]\n");
+
+    return 0;
 }
